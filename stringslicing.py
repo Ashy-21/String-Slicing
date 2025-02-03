43ffaebc4851ad05ba83add3ba468288250ee5c5
Asish Shaji <asishshaji33@gmail.com>
2025-02-03 21:50:08 +0530

    Add files via upload

---
+++ stringslicing.py
@@ -0,0 +1,4 @@
+#program for demonstrating string slicing
+str = input("Enter string:\t\n")                          # input from user
+print("\n Entered string: ",str)                         # inputed string
+print("\n Sliced string: ",str[1:5:1])                   # sliced string
